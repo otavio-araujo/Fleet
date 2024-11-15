377efad3ace4484ae0c64db7dbf2924968e307b0
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-15 10:56:12 -0300

    styled components basic configuration

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import { StatusBar } from "expo-status-bar"
+
+import theme from "@/theme"
+import { ThemeProvider } from "styled-components/native"
+
 import { SignIn } from "@/screens/SignIn"
 
 export default function App() {
   return (
     <>
-      <StatusBar style="dark" translucent backgroundColor="transparent" />
-      <SignIn />
+      <ThemeProvider theme={theme}>
+        <StatusBar style="dark" translucent backgroundColor="transparent" />
+        <SignIn />
+      </ThemeProvider>
     </>
   )
 }
