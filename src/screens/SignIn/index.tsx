5d684a11b935c5dcf0df54d4784b4f42a30425c7
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-15 10:33:30 -0300

    Basic folder structure and paths

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+import { styles } from "./styles"
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
