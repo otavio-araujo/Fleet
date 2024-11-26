4afecabd1821865d1189e48ef34fd1983581a586
Otávio Araújo <otavio_araujo@hotmail.com>
2024-11-26 17:00:01 -0300

    small changes

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 import backgroundImage from "@/assets/background.png"
 
 import { Container, Title, Slogan } from "./styles"
 import { Button } from "@/components/Button"
+import { GoogleSignin } from "@react-native-google-signin/google-signin"
+import { Alert } from "react-native"
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+})
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true)
+      const { idToken } = await GoogleSignin.signIn()
+      if (idToken) {
+      } else {
+        Alert.alert("Google Login", "Não foi possível conectar a conta Google")
+        setIsAuthenticating(false)
+      }
+    } catch (error) {
+      console.log(error)
+      setIsAuthenticating(false)
+      Alert.alert("Google Login", "Não foi possível conectar a conta Google")
+    }
+  }
   return (
     <Container source={backgroundImage}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button label="Entrar com Google" />
+      <Button
+        label="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   )
 }
